513a8506264e41650013685ae628f7595d09849a
CorruptedEyes <oviciado65@gmail.com>
2026-09-21 17:16:03 -0300

    menuzin massa

--- Assets/Scripts/MenuButtonScript.cs
+++ Assets/Scripts/MenuButtonScript.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 
 public class MenuButtonScript : MonoBehaviour
 {
+    bool openUpMotherFucker = false;
+    bool isConfigShowing = false;
+
     public void MenuShow(GameObject menu){
-        menu.SetActive(!menu.activeSelf);
+        openUpMotherFucker = !openUpMotherFucker;
+        menu.SetActive(openUpMotherFucker);
+    }
+
+    public void ConfigShow(GameObject config)
+    {
+        isConfigShowing = !isConfigShowing;
+        config.SetActive(isConfigShowing);
+    }
+
+    public void ForceCloseConfig(GameObject config)
+    {
+        isConfigShowing = false;
+        config.SetActive(isConfigShowing);
     }
 }
