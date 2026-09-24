188a3eaf8e6e2752490ece5f4992090e11fbe740
CorruptedEyes <oviciado65@gmail.com>
2026-09-24 18:46:58 -0300

    testes com Figma + GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Valores")]
+    public float money;
+    public int handwork;
+    public int trash;
+    public float lifeQuality;
+
+    [Header("UI")]
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI handworkText;
+    public TextMeshProUGUI trashText;
+
+}
